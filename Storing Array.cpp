e1dde7389840752ed84f894b6bbfe6980c918834
Akashraiky <65762219+Akashraiky@users.noreply.github.com>
2020-07-11 23:39:42 +0530

    Create Storing Array.cpp

---
+++ Storing Array.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int i,m[10];
+    cout<<"Enter Marks of 10 Students :"<<endl;
+    for(i=0;i<10;i++)
+    {
+       cin>>m[i];
+    }
+    cout<<"marks are stored"<<endl;
+    getch();
+}
